ab244b28c5d3d4273cd1736aa1438701a1db7159
AngellyC07 <110548832+AngellyC07@users.noreply.github.com>
2025-04-28 03:06:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.markdown("""
 </center>
 """, unsafe_allow_html=True)
 
-model_path = "https://github.com/AngellyC07/An-lisis_Sentimientos/blob/main/model.safetensors"  # Especifica la ruta del directorio que contiene el archivo .safetensors
+model_path = "AngellyCris/An-lisis_Sentimientos"
 model = AutoModelForSequenceClassification.from_pretrained(model_path, use_safetensors=True)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
