480ac005ac26551e20c6342c1e7e9154dcd3fa72
AngellyC07 <110548832+AngellyC07@users.noreply.github.com>
2025-04-28 05:09:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.markdown("""
 </center>
 """, unsafe_allow_html=True)
 
-model_path = "AngellyCris/An-lisis_Sentimientos"
+model_path = "AngellyCris/model.savetensors"
 model = AutoModelForSequenceClassification.from_pretrained(model_path, use_safetensors=True)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
