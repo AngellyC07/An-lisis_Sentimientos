af56829912c469e6ec1093717324898c981610e7
AngellyC07 <110548832+AngellyC07@users.noreply.github.com>
2025-04-28 05:12:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.markdown("""
 </center>
 """, unsafe_allow_html=True)
 
-model_path = "AngellyCris/model.savetensors"
+model_path = "AngellyCris/model.safetensors"
 model = AutoModelForSequenceClassification.from_pretrained(model_path, use_safetensors=True)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
